fcf299f0495df9a418833d3e2f6ed44326a5e05f
Michael Smith <msmith@msmith.id.au>
2018-09-15 22:51:10 +1000

    Exclude test files from gulp processing.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 
 gulp.task("default", function () {
-  return gulp.src("src/**/*.js")
+  return gulp.src(["src/**/*.js", "!src/**/*.test.js"])
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write("."))
